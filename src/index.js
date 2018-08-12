712a7c60023a1e92565ca21654d59d9139531567
lixing <lixingop@163.com>
2018-08-12 17:19:45 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Button} from 'antd-mobile'
+import App from './App'
+
+ReactDOM.render(<Button type='primary'>硅谷直聘</Button>,
+  document.getElementById('root'));
